6739ccdf17c6bdd167128c86de4372208aae8363
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2022-11-08 17:57:23 -0500

    Hand of god

--- ball_gravity.py
+++ ball_gravity.py
@@ -2,8 +2,8 @@ from abc import abstractmethod
 from copy import deepcopy
 import math
 import random
-import string
-from tkinter import ttk, Tk 
+from tkinter import ttk, Tk
+from typing import overload 
 from vect2d import Vect2D
 from PIL import Image, ImageTk, ImageDraw
 
@@ -13,9 +13,7 @@ from PIL import Image, ImageTk, ImageDraw
     
 
 class Updatable():
-    def __init__(self):
-        pass
-    
+
     @abstractmethod
     def tick(self):
         """Méthode abstraite qui sera redéfinie pour les balles et le jeu
@@ -31,7 +29,7 @@ class Gravitational():
         return self.__masse
      
     @abstractmethod
-    def pull(self, ball):
+    def pulled_by(self, ball):
         pass
     
 class GravityMagnet(Gravitational, Updatable):
@@ -59,9 +57,26 @@ class GravityMagnet(Gravitational, Updatable):
             else:
                 self.__direction.x = self.__directions[event.keysym.upper()].x
 
-    def pull(self):
+    def pulled_by(self):
         return self.masse * self.__direction
 
+class Trail(Updatable):
+    def __init__(self, length=15):
+        self.__length = length
+        self.__points = ([])
+        
+    def tick(self, ball):
+        self.update_trail(ball)
+        
+    def update_trail(self, ball):
+        if len(self.__points) == self.__length:
+            self.__points.pop(0)
+        
+        self.__points.append(Vect2D(ball.position.x, ball.position.y))
+
+    @property
+    def points(self):
+        return self.__points
     
 
 class Ball(Updatable, Gravitational):
@@ -77,6 +92,7 @@ class Ball(Updatable, Gravitational):
         self.__acceleration = acceleration
         self.__bounce = bounce
         self.__friction = friction
+        self.__trail = Trail();
 
     def move(self, time):
         self.__position.x += self.__speed.x + 0.5 * self.__acceleration.x * time **2
@@ -110,24 +126,38 @@ class Ball(Updatable, Gravitational):
             self.__speed.x *= self.__friction
             self.__position.y = 2.0 * (border - self.__radius) - self.__position.y
 
-    def tick(self, time, game_dimensions, acceleration):
+    def tick(self, time, game_dimensions, acceleration, game):
+        
         self.__acceleration = acceleration
         
-        if acceleration == Vect2D(0, 0):
-            #self.reset_speed()
-            pass
+        if not game.hand_of_god == Vect2D(-1, -1):
+            self.pushed_by(game.hand_of_god)
+            
+        if game.gravity_field_active:
+                self.pulled_by(game.balls)
+                
+        # if acceleration == Vect2D(0, 0):
+        #     self.reset_speed()
+        #     pass
 
         self.move(time)
         self.bounce(game_dimensions)
          
             
-    def pull(self, ball):
-        pass
+    def pulled_by(self, balls):
+        for ball in balls:
+            if ball is not self:
+                self.__acceleration += ball.masse * (ball.position - self.position) / ((ball.position - self.position).length_squared + 100 ** 2) ** 1.5            
 
+    def pushed_by(self, hand_of_god):
+        self.__acceleration += -10000000 * (hand_of_god - self.position) / ((hand_of_god - self.position).length_squared + 100 ** 2) ** 1.5
+        print(self.__acceleration) 
+        pass  
+    
     def reset_speed(self):
         self.__speed.x = self.__initial_speed.x
         self.__speed.y = self.__initial_speed.y
-        pass
+        
 
     @property
     def position(self):
@@ -148,23 +178,43 @@ class Ball(Updatable, Gravitational):
     @property
     def density(self):
         return self.__density
+    
+    @property
+    def trail(self):
+        return self.__trail
         
 class Game(Updatable):
 
-    def __init__(self, size, nb_balls=100):
+    def __init__(self, size, nb_balls=90):
         super().__init__()
         self.__gravity_magnet = GravityMagnet()
         self.__size = size
         self.__nb_balls = nb_balls
         self.__balls = []
+        self.__gravity_field_active = False
+        self.__hand_of_god = Vect2D(-1, -1)
         for _ in range(self.__nb_balls):
-            self.__balls.append(Ball(radius=random.randrange(5,25), fill_color=(random.randint(0,255),random.randint(0,255), random.randint(0,255)), border_color=(random.randint(0,255),random.randint(0,255), random.randint(0,255)), density=random.randrange(0,10), position=Vect2D(random.randrange(0,self.__size.x),random.randrange(0,self.__size.y)), speed=Vect2D(random.randrange(-10,10),random.randrange(-10,10)), acceleration=Vect2D(0,0), bounce=0.95, friction=0.95))
+            self.__balls.append(Ball(radius=random.randrange(5,50), fill_color=(random.randint(0,255),random.randint(0,255), random.randint(0,255)), border_color=(random.randint(0,255),random.randint(0,255), random.randint(0,255)), density=50, position=Vect2D(random.randrange(0,self.__size.x),random.randrange(0,self.__size.y)), speed=Vect2D(random.randrange(-10,10),random.randrange(-10,10)), acceleration=Vect2D(0,0), bounce=0.95, friction=0.95))
             #self.__balls.append(Ball())
 
     def tick(self):
         for ball in self.__balls:
-            ball.tick(0.01, self.__size, self.gravity_magnet.pull())
+            ball.tick(0.01, self.__size, self.gravity_magnet.pulled_by(), self)
+                    
 
+    def toggle_gravity_field(self, event):
+        if self.__gravity_field_active:
+            self.__gravity_field_active = False
+        else:
+            self.__gravity_field_active = True
+            
+    def move_hand_of_god(self, event):
+        self.__hand_of_god.x = event.x
+        self.__hand_of_god.y = event.y
+
+    @property
+    def gravity_field_active(self):
+        return self.__gravity_field_active
 
     @property
     def gravity_magnet(self):
@@ -173,6 +223,10 @@ class Game(Updatable):
     @property
     def balls(self):
         return self.__balls
+    
+    @property
+    def hand_of_god(self):
+        return self.__hand_of_god
 
 class Application(Tk, Updatable):
     
@@ -204,6 +258,9 @@ class Application(Tk, Updatable):
         self.bind('<KeyRelease-Down>', self.g.gravity_magnet.set_direction)
         self.bind('<KeyRelease-Up>', self.g.gravity_magnet.set_direction)
 
+        self.bind('<KeyPress-Return>', self.g.toggle_gravity_field)
+        
+        self.bind('<Motion>', self.g.move_hand_of_god)
 
         self.tick()
 
@@ -222,13 +279,16 @@ class Application(Tk, Updatable):
 
         for ball in self.g.balls:
             draw.ellipse([(ball.position.x - ball.radius, ball.position.y - ball.radius), (ball.position.x + ball.radius, ball.position.y + ball.radius)], ball.fill_color, ball.border_color)
+            ball.trail.tick(ball)
+            for point in ball.trail.points:
+                draw.point((point.x, point.y), ball.fill_color)
        
         self.tki = ImageTk.PhotoImage(i)
         self.w["image"] = self.tki
 
 def main():
     
-    app = Application(500,500)
+    app = Application(1000,1000)
     app.mainloop()
     
 if __name__ == '__main__':
